7c20d1eef31daf69b31c4094cae8886c9e3e134e
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2020-02-17 09:13:39 -0500

    Removed target directories from maven build and updated the example servlet test.

--- packages/storybook-aem-foundation/core/src/test/java/org/icfnext/storybookaem/servlets/StorybookProxyServletTest.java
+++ packages/storybook-aem-foundation/core/src/test/java/org/icfnext/storybookaem/servlets/StorybookProxyServletTest.java
@@ -9,29 +9,20 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 public class StorybookProxyServletTest {
-    @Test public void testDisabledAlias() throws Exception {
+    @Test public void testExample() throws Exception {
         /*
-        final DefaultGetServlet servlet = new DefaultGetServlet();
-        final DefaultGetServlet.Config config = Mockito.mock(DefaultGetServlet.Config.class);
-        Mockito.when(config.enable_html()).thenReturn(true);
-        Mockito.when(config.enable_json()).thenReturn(true);
-        Mockito.when(config.enable_xml()).thenReturn(false);
-        Mockito.when(config.enable_txt()).thenReturn(false);
-        Mockito.when(config.aliases()).thenReturn(new String[] {"xml:pdf"});
+        final DefaultGetServlet servlet = new StorybookProxyServlet();
+        final DefaultGetServlet.Config config = Mockito.mock(StorybookProxyServlet.Config.class);
+        Mockito.when(config.example()).thenReturn(true);
         servlet.activate(config);
-
         servlet.init();
 
-        final Field field = DefaultGetServlet.class.getDeclaredField("rendererMap");
+        final Field field = DefaultGetServlet.class.getDeclaredField("somefield");
         field.setAccessible(true);
 
         @SuppressWarnings("unchecked")
         final Map<String, Servlet> map = (Map<String, Servlet>) field.get(servlet);
         assertEquals(4, map.size());
-        assertNotNull(map.get("res"));
-        assertNotNull(map.get("html"));
-        assertNotNull(map.get("json"));
-        assertNotNull(map.get("pdf"));
         */
     }
 }
