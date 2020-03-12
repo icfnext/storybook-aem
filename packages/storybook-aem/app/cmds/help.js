991059233b2211a983355d5607e67433e1771b90
jzeltman <joshua.zeltman@gmail.com>
2020-03-12 18:47:23 -0300

    Updated documentation

--- packages/storybook-aem/app/cmds/help.js
+++ packages/storybook-aem/app/cmds/help.js
@@ -4,9 +4,9 @@ storybook-aem [command] <options>
 
 Commands:
   init .................. Start a new project, or add to existing project
-  story, stories ........ Creates a new component in your project. Generates Storybook files in the component folder specified by the name
-  component ............. Create a new component in your project. Generates files in the specified component folder
-  content ............... Create AEM Content saved in the JCR from [component].content.js files
+  story ................. Creates/Updates your component story file, Adds story definition, Creates AEM Content example
+  component ............. WIP - Create a new component in your project. Generates files in the specified component folder
+  content ............... WIP - Create AEM Content saved in the JCR from [component].content.js files
   help .................. Show help menu for storybook-aem
   version, v ............ Show storybook-aem version
 `,
@@ -15,9 +15,20 @@ init: `
 storybook-aem init`,
 
 story: `
-storybook-aem story <options>
+storybook-aem story
 
---location, -l ..... the location to use`,
+- Use this command during your development or documentation process
+- Configure for which component type you want to make stories 
+- Select component
+- Add a list of stories
+-- By default, when creating a new story file, an empty story will be created for you
+- Choose to create content in AEM to correspond to the story
+
+Upon completing the configuration, a story file will be created or updated with the list of stories provided.
+Content in AEM will also be created corresponding to each story.
+The story file will be updated with the AEM Content Path created for each story component, and denoted with a 
+ heading that contains the name of the story.
+`,
 
 component: `
 storybook-aem component <options>
@@ -28,7 +39,7 @@ content: `
 storybook-aem component <options>
 
 --location, -l ..... the location to use`
-  }
+}
   
 module.exports = (args) => {
     const subCmd = args._[0] === 'help'
