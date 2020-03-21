75f885df27eb225735e7ded11ec1fb8bc3f90681
jzeltman <joshua.zeltman@gmail.com>
2020-03-21 10:55:08 -0300

    WIP SS updates

--- packages/storybook-aem-style-system/src/index.js
+++ packages/storybook-aem-style-system/src/index.js
@@ -6,6 +6,8 @@ let classes = 'testing-style-system';
 channel.on('aemStyleSystem:update', event => StyleSystem(event.classes));
 
 export const StyleSystem = (value) => {
+    console.log('value:', value)
     if (value !== undefined) classes = value;
+    console.log('classes:', classes)
     return classes;
 }
