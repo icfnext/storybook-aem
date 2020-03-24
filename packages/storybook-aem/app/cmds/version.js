ea8f00214d9ffc7c85a52acc8308b9fc0dfa58b4
jzeltman <joshua.zeltman@gmail.com>
2020-03-24 15:05:53 -0300

    Updated the version command to check installed version vs latest

--- packages/storybook-aem/app/cmds/version.js
+++ packages/storybook-aem/app/cmds/version.js
@@ -1,4 +1,18 @@
+const npm = require('npm');
+const chalk = require('chalk');
+
 const { version } = require('../../package.json');
 const log = require('../utils/logger');
 
-module.exports = (args) => log(`v${version}`);
+module.exports = (args) => {
+    npm.load({ loaded: false }, (err) => {
+        if (err) throw err;
+        
+        npm.commands.view(['storybook-aem'],true,(a,json,c) => {
+            let latest = Object.keys(json)[0];
+            let color = latest >= version ? chalk.green : chalk.red;
+            log(`Latest:    v${Object.keys(json)[0]}`);
+            log(color(`Installed: v${version}`));
+        });
+    });
+}
