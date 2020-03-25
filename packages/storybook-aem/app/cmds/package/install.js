d2d7e105296c3b956712544426c71b06719932a0
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2020-03-25 11:52:58 -0400

    Unziping content package after export and zipping package before import.

--- packages/storybook-aem/app/cmds/package/install.js
+++ packages/storybook-aem/app/cmds/package/install.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const FormData = require('form-data');
-const { exec } = require('child_process');
-
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const chalk = require('chalk');
+const zip = require('zip-promise');
 const cwd = process.cwd();
 const log = require('../../utils/logger');
 const fetchFromAEM = require('../../utils/fetchFromAEM');
@@ -10,30 +12,41 @@ const fetchFromAEM = require('../../utils/fetchFromAEM');
 module.exports = async (args,config) => {
     log(`Checking for Storybook AEM Content Package configuration...`);
     const packageManagerURL = `/crx/packmgr/service.jsp`;
-    const storybookPackagePath = path.resolve(cwd, config.projectRoot, config.relativeProjectRoot, config.localPackagePath, config.packageName);
-    if (!fs.existsSync(storybookPackagePath)) {
-        throw log(chalk.red(`error, could not find Storybook AEM Content Package`));
-    }
+    const storybookPackageDirectory = path.resolve(cwd, config.projectRoot, config.relativeProjectRoot, config.localPackagePath);
+    const storybookPackagePath = path.resolve(storybookPackageDirectory, config.packageName);
 
-    const form = new FormData();
-    form.append('file', fs.createReadStream(storybookPackagePath));
-    form.append('name', `/packages/` + config.packageName);
-    form.append('force', 'true');
-    form.append('install', 'true');
+    try {
+      await zip.folder(storybookPackageDirectory, storybookPackagePath);
 
-    log(`Attempting to create Storybook AEM Content Package in AEM...`);
-    await fetchFromAEM({
-        url: packageManagerURL,
-        method: 'POST',
-        body: form
-    });
-    log(`Storybook AEM Content Package successfully created in AEM!`);
-    log([
-        `You can see the Storybook AEM Content here:\n`,
-        `  http://localhost:4502/sites.html${config.aemContentPath}\n`
-    ].join('\n'));
-    
-    if (!args.includes('--quiet')) {
-        exec(`open http://localhost:4502/sites.html${config.aemContentPath}`);
+      if (!fs.existsSync(storybookPackagePath)) {
+          throw log(chalk.red(`error, could not find Storybook AEM Content Package`));
+      }
+
+      const form = new FormData();
+      form.append('file', fs.createReadStream(storybookPackagePath));
+      form.append('name', `/packages/` + encodeURIComponent(config.packageName));
+      form.append('force', 'true');
+      form.append('install', 'true');
+
+      log(`Installing Storybook AEM Content Package to AEM...`);
+      await fetchFromAEM({
+          url: packageManagerURL,
+          method: 'POST',
+          body: form
+      });
+
+      log(`Cleaning up...`);
+      await exec(`rm -rf "${storybookPackagePath}"`);
+
+      log([
+          `You can see the Storybook AEM Content here:\n`,
+          `  http://localhost:4502/sites.html${config.aemContentPath}\n`
+      ].join('\n'));
+
+      if (!args.includes('--quiet')) {
+          exec(`open http://localhost:4502/sites.html${config.aemContentPath}`);
+      }
+    } catch (e) {
+      throw log("There was an error installing the content package", chalk.red(e));
     }
 };
