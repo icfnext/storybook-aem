f2c62ec63ac6c28dfe68de3d5bc6233d176b1d78
jzeltman <joshua.zeltman@gmail.com>
2020-03-26 18:05:15 -0300

    Added boxen for pretty version checking

--- packages/storybook-aem/app/utils/versionCheck.js
+++ packages/storybook-aem/app/utils/versionCheck.js
@@ -1,5 +1,6 @@
 const npm = require('npm');
 const chalk = require('chalk');
+const boxen = require('boxen');
 
 const { version } = require('../../package.json');
 
@@ -20,19 +21,23 @@ async function checkVersion() {
     const installed = getInstalledVersion();
     getLatestVersion(latest => {
         if (latest > installed) {
-            const sb = chalk.rgb(255, 71, 133).bold;
-            console.log([
-                ``,
-                `  ${sb(`---------------------------------------------`)}`,
-                `  ${sb(`|`)}                                           ${sb(`|`)}`,
-                `  ${sb(`|`)}  There's a ${chalk.green(`new version`)} of ${chalk.rgb(255, 71, 133).bold('storybook-aem')}!  ${sb(`|`)}`,
-                `  ${sb(`|`)}                                           ${sb(`|`)}`,
-                `  ${sb(`|`)}    ${chalk.bold(`Latest:`)}    v${latest}                      ${sb(`|`)}`,
-                `  ${sb(`|`)}    ${chalk.red.bold(`Installed: v${installed}`)}                      ${sb(`|`)}`,
-                `  ${sb(`|`)}                                           ${sb(`|`)}`,
-                `  ${sb(`---------------------------------------------`)}`,
-                ``
-            ].join('\n'))
+            const message = `There's a ${chalk.green(`new version`)} of ${chalk.rgb(255, 71, 133).bold('storybook-aem')}!`;
+            const latestMessage = `${chalk.green(`Latest: v${latest}`)}`;
+            const installedMessage = `${chalk.red(`Installed: v${installed}`)}`;
+            const updateWith = chalk.bold.dim('Update with either:');
+            console.log(
+                boxen([
+                    `${message}`,
+                    ``,
+                    `  ${latestMessage} ${chalk.dim('-')} ${installedMessage}`,
+                    `  `,
+                    ``,
+                    `          ${updateWith}`,
+                    `      npm update -g storybook-aem`,
+                    `                 ${chalk.dim('or')}`,
+                    `   yarn global upgrade storybook-aem`,
+                ].join('\n'), { padding: 1, margin: 1, borderColor: '#F1618C', borderStyle: 'round' })
+            );
         }
     });
 }
