24e9fb0ec2fb1991898ad19725b3ac7e013cea2c
jzeltman <joshua.zeltman@gmail.com>
2020-04-02 12:05:02 -0300

    wip

--- packages/storybook-aem-style-system/src/index.js
+++ packages/storybook-aem-style-system/src/index.js
@@ -1,11 +1,17 @@
 import addons from '@storybook/addons';
+import { useParameter } from '@storybook/api';
+import { FORCE_RE_RENDER, STORY_CHANGED } from '@storybook/core-events';
+import { ADDON_ID, PANEL_ID, PARAM_KEY } from './constants';
 
 const channel = addons.getChannel();
 let classes = 'testing-style-system';
 
 channel.on('aemStyleSystem:update', event => StyleSystem(event.classes));
+channel.on(STORY_CHANGED, (event,b,c) => console.log('STORY_CHANGED',event,b,c));
 
 export const StyleSystem = (value) => {
+    // const parameters = useParameter(PARAM_KEY, null);
+
     console.log('value:', value)
     if (value !== undefined) classes = value;
     console.log('classes:', classes)
